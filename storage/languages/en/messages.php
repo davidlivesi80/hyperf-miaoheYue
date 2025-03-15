cd33fd81749163312da89c0f11aad7604d14e878
davidlivesi80 <davidlivesi80@gmail.com>
2025-03-15 13:32:34 +0800

    1

---
+++ storage/languages/en/messages.php
@@ -0,0 +1,93 @@
+<?php
+
+// storage/languages/en/messages.php
+return [
+    //共有
+    'success'   => 'success',
+    'fail'      => 'fail',
+    'edit_success' => 'edit success',
+    'edit_fail' => 'edit fail',
+    'operate_success' => 'operate success',
+    'operate_fail' => 'operate fail',
+    'verify_success' => 'verify success',
+    'verify_fail' => 'verify success',
+    'upload_success' => 'upload success',
+    'upload_fail' => 'upload fail',
+    'upload_error'=>'upload error',
+    'param_error'=>'param error',
+    'data_error'=>'data error',
+    'insufficient_balance'=>'insufficient balance',
+    'try_later'=>'try later',
+    'data_error_1'=>'data error！',
+    'data_error_2'=>'data error！！',
+    'order_tip'=>'you have submitted an order to be paid, waiting to be processed',
+    'widthraw_auth' => 'Withdrawal rights are not enabled, please contact the administrator',
+    //用户、钱包
+    'paysword_error' => 'Wrong paysword',
+    'password_error' => 'Wrong password',
+    'wallet_id' => 'Wallet ID already exists, please fill it in again',
+    'bind_tip' => 'Email or Google is not bound',
+    'code_tip' => 'code error',
+    'spread_no' => 'The promoter does not exist',
+    'spread_jihuo' => 'Promoter is not activated',
+    'network_01' => 'Network abnormality, try later!',
+    'network_02' => 'Network abnormality, try later!!',
+    'network_03' => 'Network abnormality, creation failed',
+    'help_str' => 'Mnemonic phrase is wrong, please check and re-enter',
+    'paysword_set' => 'Please complete the payment password setting first',
+    //资产
+    'recharge_on' => 'Recharge is not enabled',
+    'recharge_time' => 'Recharge time:',
+    'withdraw_on' => 'Withdrawal is not enabled',
+    'withdraw_num' => 'Withdrawal quantity:',
+    'withdraw_time' => 'Withdrawal time:',
+    'tranfer_on' => 'Transfer is not enabled',
+    'tranfer_time' => 'Transfer time:',
+    //兑换
+    'exchange_num' => 'Single exchange range：',
+    //互转
+    'target_error'=>'The other party does not exist',
+    //otc
+    'otc_no' => 'This ad does not exist',
+    'otc_down' => 'This ad has been taken offline',
+    'otc_order' => 'This ad cannot be ordered',
+    'otc_order_max' => 'The maximum order for this advertisement is',
+    'otc_order_min' => 'The minimum order for this advertisement is',
+    'otc_exits' => 'Transaction does not exist',
+    'otc_close' => 'Transaction closed',
+    'otc_init' => 'Transaction not initialized',
+    'otc_pub_num' => 'Maximum order quantity exceeded',
+    'otc_order_qiang' => 'This ad has been snapped up',
+    'otc_disabe' => 'Please disable ads first',
+    'otc_disabe_fail' => 'Disable failed!!',
+    'otc_enable' => 'Please enable ads first!',
+    'otc_enable_fail' => 'Failed to enable!!',
+    'otc_remove' => 'Please cancel the ad first',
+    'otc_remove_fail' => 'Undo failed',
+    //报单
+    'pool_set' => 'Configuration does not take effect',
+    'pool_min' => 'minimum investment',
+    //节点
+    'node_exits' => 'Already a node user',
+    'node_rule' => 'conditions not met',
+    //中间件
+    'huakuai_guoqi' => 'Slider signature expired',
+    'huakuai_error' => 'Slider signature error',
+    'ip_error' => 'Illegal IP access',
+    'req_error' => 'Illegal request',
+    'req_token' => 'req-token error',
+    'sign_01' => 'Signature parameter error',
+    'sign_02' => 'Signature verification failed',
+    //其他
+    'invalid_check'=>'Invalid check',
+    'token_01' => 'token wrong format',
+    'token_02' => 'Caching token failed',
+    'token_03' => 'token cannot be empty',
+    'token_04' => 'Your account is logged in from another party, please log in again',
+    'token_05' => 'Incorrect login status, please log in again',
+    //短信
+    'sms_01' => 'Failed to send',
+    'sms_02' => 'Sent successfully',
+
+
+];
